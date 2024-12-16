70ce4fe4402f2593889e7fbb489342a64aff90b4
shota-08 <kame.s.0828@icloud.com>
2024-12-16 12:51:48 +0900

    update : sqlite セットアップ

---
+++ sql_app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 同じディレクトリにDB作成
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+# インスタンス
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # 接続設定
+Base = declarative_base() # クラスの継承
